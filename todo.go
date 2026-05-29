19c99ff1e23f8cea456a91fc71d5cae6a685c99f
anushacodes <anusha.nandy@gmail.com>
2026-05-29 00:57:29 -0500

    added delete method

--- todo.go
+++ todo.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Todo struct {
 	Title       string
@@ -10,3 +13,39 @@ type Todo struct {
 }
 
 type Todos []Todo //like Todos = Todo[] in py
+
+// methods
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedAt: nil,
+		CreatedAt:   time.Now(),
+	}
+
+	// appending todo to it's struct
+	*todos = append(*todos, todo)
+}
+
+// helper emthod to find index of todo by title
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+
+		// err := errors.New("invalid index")
+		err := fmt.Errorf("invalid index") //check diff?
+		fmt.Println(err)
+	}
+
+	return nil
+}
+
+// delete function - we split at del index then join again after removing that index
+func (todo *Todos) delete(index int) error {
+	t := *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+	*todo = append(t[:index], t[index+1:]...)
+	return nil
+
+}
